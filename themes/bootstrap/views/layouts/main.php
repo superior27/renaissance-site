40184d414fefd50ef5e9981f6ad39ac1d1275be7
lucas <lucas.fsl@hotmail.com>
2014-03-17 09:14:11 -0300

    Index pronta

--- themes/bootstrap/views/layouts/main.php
+++ themes/bootstrap/views/layouts/main.php
@@ -1,78 +1,82 @@
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt-br" lang="pt-br">
 
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
-   
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <head>
+    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="language" content="pt-br" />
+		<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+		
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-	<?php Yii::app()->bootstrap->register(); ?>
-</head>
+        <!-- CSS -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,400">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Droid+Sans">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster">
+        <link rel="stylesheet" href="themes/assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="themes/assets/prettyPhoto/css/prettyPhoto.css">
+        <link rel="stylesheet" href="themes/assets/css/flexslider.css">
+        <link rel="stylesheet" href="themes/assets/css/font-awesome.css">
+        <link rel="stylesheet" href="themes/assets/css/style.css">
 
-<body>
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
 
-<?php $this->widget('bootstrap.widgets.TbNavbar',array(
-    
-    'collapse'=>'true',
-    'items'=>array(
-        array(
-            'class'=>'bootstrap.widgets.TbMenu',
-            'items'=>array(
-                array('label'=>'Home', 'url'=>array('/')),
-                array('label'=>'Sobre', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Usuário', 'url'=>array('/user/index')),				
-                array('label'=>'Contato', 'url'=>array('/site/contact')),
-                array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-                array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-            ),
-        ),
-        '<form class="navbar-search pull-right" action=""><input type="text" class="search-query span2" placeholder="Search"></form>',
-    ),
-)); ?>
+        <!-- Favicon and touch icons -->
+        <link rel="shortcut icon" href="themes/assets/ico/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="themes/assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="themes/assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="themes/assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="themes/assets/ico/apple-touch-icon-57-precomposed.png">
+
+    </head>
+
+
+<!--
+<? /*php $this->widget('bootstrap.widgets.TbNavbar',array(
+
+'collapse'=>'true',
+'items'=>array(
+array(
+'class'=>'bootstrap.widgets.TbMenu',
+'items'=>array(
+array('label'=>'Home', 'url'=>array('/')),
+array('label'=>'Sobre', 'url'=>array('/site/page', 'view'=>'about')),
+array('label'=>'Usuário', 'url'=>array('/user/index')),				
+array('label'=>'Contato', 'url'=>array('/site/contact')),
+array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+),
+),
+'<form class="navbar-search pull-right" action=""><input type="text" class="search-query span2" placeholder="Search"></form>',
+),
+)); */?>
 <div class="hero-unit1">
-	<div class="span2 img-circle">
-		<?php echo CHtml::image(Yii::app()->request->baseUrl.'/images/logo.jpg');?>
-	
+<div class="span2 img-circle">
+<?php echo CHtml::image(Yii::app()->request->baseUrl.'/images/logo.jpg');?>
+
 </div>
 </br>
 
-  <h1>Renascença</h1>
-  
-</div>
+<h1>Renascença</h1>
 
-<div class="container" id="page">
+</div>-->
 
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
-
-	<?php echo $content; ?>
-
-	<div class="clear"></div>
-	</br>
-	</br>
-	</br>
-	</br>
-	</br>
-	</br>
+<body>
 	
+				<?php if(isset($this->breadcrumbs)):?>
+				<?php $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
+					'links'=>$this->breadcrumbs,
+					)); ?><!-- breadcrumbs -->
+				<?php endif?>
 
-</div><!-- page -->
-<div class="footer">
-	<div class="container">
-		<div class="row-fluid">
-		
-		Criado por  <!--<?php echo date('Y'); ?>--> CommSystem.<br/>
-		Todos os direitos reservados.<br/>
-		<?php echo Yii::powered(); ?>
-	
-	</div>
-	</div>
-	</div><!-- footer -->
+				<?php echo $content; ?>
+
+				
+
 </body>
 </html>
--- themes/bootstrap/views/site/index.php
+++ themes/bootstrap/views/site/index.php
@@ -1,104 +1,262 @@
-<?php
-/* @var $this SiteController */
-
-$this->pageTitle=Yii::app()->name;
-?>
-<!--<?php echo CHtml::image(Yii::app()->request->baseUrl.'/images/Black-and-White-Lace.jpg');?>
-<?php $this->beginWidget('bootstrap.widgets.TbHeroUnit',array(
-    'heading'=>CHtml::encode(Yii::app()->name),
-)); ?>
-<?php $this->endWidget(); ?>-->
-<!--<p>Congratulations! You have successfully created your Yii application.</p>
--->
-
-<!--
-<p>You may change the content of this page by modifying the following two files:</p>
-
-<ul>
-    <li>View file: <code><?php echo __FILE__; ?></code></li>
-    <li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-    the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-    Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-    should you have any questions.</p>
-
--->
-<div class="row-fluid">
-	
-	<div class="span12" >
-<?php $this->widget('bootstrap.widgets.TbCarousel', array(
-    'items'=>array(
-        array('image'=>'images/modelo8.png', 'label'=>'First Thumbnail label', 'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'),
-        array('image'=>'images/modelo6.jpg', 'label'=>'Second Thumbnail label', 'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'),
-        array('image'=>'images/modelo4.jpg', 'label'=>'Third Thumbnail label', 'caption'=>'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'),
-    ),
-    'htmlOptions'=>array('width'=>'50%','height'=>'10%')
-)); ?>
-</div>
-<div class="offset2"></div>
-</div>
 
 
-<div class="row-fluid">
-	<div class="span12">
-	
-	<div class="span4  alert alert-info">
-		<a href="index.php/site/page?view=about" title="Sobre">
-				<img src ="images/info.png" class="span3">
-			</a> 
-		<h2>
-			Sobre
-		</h2>
-	</br>
-		<p>
-			//Criar um texto aqui para falar um pouco sobre o "soubre"
-			</p>
-			
-		
-	</div>
 
-	<div class="span4 alert alert-info">
-		<a href="index.php/site/page?view=about" title="Vitrine">
-				<img src ="images/modelos1.png" class="span3">
-		</a> 
-		<h2>
-			Vitrine
-		</h2>
-	</br>
-		<p>
-			Fique por dentro dos nossos principais modelos
-			//incrementar com um texto um pouco maior
-		</p>
-	</div>
+    <body>
 
-	<div class="span4 alert alert-info">
-		<h1>
-			Redes Sociais
-		</h1>
-		
-		<p>
-			<a href="http://facebook.com" rel="external" title="Facebook">
-				<img src ="images/facebook2.png" class="span3">
-			</a>
-		
+        <!-- Header -->
+        <div class="container">
+            <div class="header row">
+                <div class="span12">
+                    <div class="navbar">
+                        <div class="navbar-inner">
+                            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </a>
+                            <h1>
+                                <a class="brand" href="index.html">Andia - a super cool design agency...</a>
+                            </h1>
+                            <div class="nav-collapse collapse">
+                                <ul class="nav pull-right">
+                                    <li class="current-page">
+                                        <a href="index.html"><i class="icon-home"></i><br />Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="icon-camera"></i><br />Portfolio</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="icon-comments"></i><br />Blog</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="icon-tasks"></i><br />Services</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="icon-user"></i><br />About</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="icon-envelope-alt"></i><br />Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-		
-			<a href="http://twitter.com" rel="external" title="Twitter">
-				<img src ="images/twitter1.png" class="span3">
-			</a>
+        <!-- Slider -->
+        <div class="slider">
+            <div class="container">
+                <div class="row">
+                    <div class="span10 offset1">
+                        <div class="flexslider">
+                            <ul class="slides">
+                                <li data-thumb="images/modelo4.jpg">
+                                    <img src="images/modelo4.jpg">
+                                    <p class="flex-caption">Amazing collection of dresses made with the best raw materials...</p>
+                                </li>
+                                <li data-thumb="images/modelo6.jpg">
+                                    <img src="images/modelo6.jpg">
+                                    <p class="flex-caption">Amazing collection of dresses made with the best raw materials...</p>
+                                </li>
+                                <li data-thumb="images/modelo8.png">
+                                    <img src="images/modelo8.png">
+                                    <p class="flex-caption">Amazing collection of dresses made with the best raw materials...</p>
+                                </li>
+                                <li data-thumb="images/modelo3.jpg">
+                                    <img src="images/modelo3.jpg">
+                                    <p class="flex-caption">Amazing collection of dresses made with the best raw materials...</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-			<a href="http://plus.google.com" rel="external" title="Google+">
-				<img src ="images/google+.png" class="span3">
-			</a>
+        <!-- Site Description -->
+        <div class="presentation container">
+            <h2>We are <span class="violet">Renascença</span>, a super cool embroidered clothing factory.</h2>
+            <p>We design beautiful dresses, shirts, skirts and so on. Your satisfaction is safe with us.</p>
+        </div>
 
-		
-			<a href="http://instagram.com" rel="external" title="Instagram">
-				<img src ="images/instagram1.png" class="span3">
-			</a>
-		</p>
-	</div>
+        <!-- Services 
+        <div class="what-we-do container">
+            <div class="row">
+                <div class="service span3">
+                    <div class="icon-awesome">
+                        <i class="icon-eye-open"></i>
+                    </div>
+                    <h4>Beautiful Websites</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
+                    <a href="services.html">Read more</a>
+                </div>
+                <div class="service span3">
+                    <div class="icon-awesome">
+                        <i class="icon-table"></i>
+                    </div>
+                    <h4>Responsive Layout</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
+                    <a href="services.html">Read more</a>
+                </div>
+                <div class="service span3">
+                    <div class="icon-awesome">
+                        <i class="icon-magic"></i>
+                    </div>
+                    <h4>Awesome Logos</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
+                    <a href="services.html">Read more</a>
+                </div>
+                <div class="service span3">
+                    <div class="icon-awesome">
+                        <i class="icon-print"></i>
+                    </div>
+                    <h4>High Res Prints</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
+                    <a href="services.html">Read more</a>
+                </div>
+            </div>
+        </div>-->
 
-</div>
-</div>
+        <!-- Latest Work -->
+        <div class="portfolio container">
+            <div class="portfolio-title">
+                <h3>Our Latest Work</h3>
+            </div>
+            <div class="row">
+                <div class="work span3">
+                    <img src="images/modelo4.jpg" alt="">
+                    <h4>Dress</h4>
+                    <p>Amazing collection of dresses made with the best raw materials....</p>
+                    <div class="icon-awesome">
+                        <a href="images/modelo4.jpg" rel="prettyPhoto"><i class="icon-search"></i></a>
+                        <a href="#"><i class="icon-link"></i></a>
+                    </div>
+                </div>
+                <div class="work span3">
+                    <img src="images/modelo6.jpg" alt="">
+                    <h4>Dress</h4>
+                    <p>Amazing collection of dresses made with the best raw materials....</p>
+                    <div class="icon-awesome">
+                        <a href="images/modelo6.jpg" rel="prettyPhoto"><i class="icon-search"></i></a>
+                        <a href="portfolio.html"><i class="icon-link"></i></a>
+                    </div>
+                </div>
+                <div class="work span3">
+                    <img src="images/modelo8.png" alt="">
+                    <h4>Dress</h4>
+                    <p>Amazing collection of dresses made with the best raw materials....</p>
+                    <div class="icon-awesome">
+                        <a href="images/modelo8.png" rel="prettyPhoto"><i class="icon-search"></i></a>
+                        <a href="portfolio.html"><i class="icon-link"></i></a>
+                    </div>
+                </div>
+                <div class="work span3">
+                    <img src="images/modelo3.jpg" alt="">
+                    <h4>Dress</h4>
+                    <p>Amazing collection of dresses made with the best raw materials....</p>
+                    <div class="icon-awesome">
+                        <a href="images/modelo3.jpg" rel="prettyPhoto"><i class="icon-search"></i></a>
+                        <a href="portfolio.html"><i class="icon-link"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Testimonials -->
+        <div class="testimonials container">
+            <div class="testimonials-title">
+                <h3>Testimony</h3>
+            </div>
+            <div class="row">
+                <div class="testimonial-list span12">
+                    <div class="tabbable tabs-below">
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="A">
+                                <img src="themes/assets/img/testimonials/1.jpg" title="" alt="">
+                                <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur..."<br /><span class="violet">Lorem Ipsum, dolor.co.uk</span></p>
+                            </div>
+                            <div class="tab-pane" id="B">
+                                <img src="themes/assets/img/testimonials/2.jpg" title="" alt="">
+                                <p>"Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat..."<br /><span class="violet">Minim Veniam, nostrud.com</span></p>
+                            </div>
+                            <div class="tab-pane" id="C">
+                                <img src="themes/assets/img/testimonials/3.jpg" title="" alt="">
+                                <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur..."<br /><span class="violet">Lorem Ipsum, dolor.co.uk</span></p>
+                            </div>
+                            <div class="tab-pane" id="D">
+                                <img src="themes/assets/img/testimonials/1.jpg" title="" alt="">
+                                <p>"Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat..."<br /><span class="violet">Minim Veniam, nostrud.com</span></p>
+                            </div>
+                        </div>
+                       <ul class="nav nav-tabs">
+                           <li class="active"><a href="#A" data-toggle="tab"></a></li>
+                           <li class=""><a href="#B" data-toggle="tab"></a></li>
+                           <li class=""><a href="#C" data-toggle="tab"></a></li>
+                           <li class=""><a href="#D" data-toggle="tab"></a></li>
+                       </ul>
+                   </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Footer -->
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="widget span3">
+                        <h4>About Us</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+                        <p><a href="">Read more...</a></p>
+                    </div>
+                    <div class="widget span3">
+                        <h4>Latest Tweets</h4>
+                        <div class="show-tweets"></div>
+                    </div>
+                    <div class="widget span3">
+                        <h4>Flickr Photos</h4>
+                        <ul class="flickr-feed"></ul>
+                    </div>
+                    
+                    <div class="widget span3">
+                        <h4>Entre em contato</h4>
+                        <p><i class="icon-map-marker"></i> Endereço: Via Principe Amedeo 9, 10100, Torino, TO, Italy</p>
+                        <p><i class="icon-phone"></i> Telefone: 0039 333 12 68 347</p>
+                        <p><i class="icon-user"></i> Skype: MyNickName</p>
+                        <p><i class="icon-envelope-alt"></i> Email: <a href="">contact@contact.com.br</a></p>
+                    </div>
+                </div>
+                <div class="footer-border"></div>
+                <div class="row">
+                    <div class="copyright span4">
+                    	
+				
+                        <p>Copyright <!--<?php echo date('Y'); ?>--> CommSystem.<br/> - All rights reserved <a href="http://azmind.com">CommSystem</a>.</p>
+                    </div>
+                    <div class="social span8">
+                        <a class="facebook" href=""></a>
+                        <a class="googleplus" href=""></a>
+                        <a class="twitter" href=""></a>
+                        <a class="youtube" href=""></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <!-- Javascript -->
+        <script src="themes/assets/js/jquery-1.8.2.min.js"></script>
+        <script src="themes/assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="themes/assets/js/jquery.flexslider.js"></script>
+        <script src="themes/assets/js/jquery.tweet.js"></script>
+        <!--<script src="themes/assets/js/jflickrfeed.js"></script>-->
+        <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        <script src="themes/assets/js/jquery.ui.map.min.js"></script>
+        <script src="themes/assets/js/jquery.quicksand.js"></script>
+        <script src="themes/assets/prettyPhoto/js/jquery.prettyPhoto.js"></script>
+        <script src="themes/assets/js/scripts.js"></script>
+
+    </body>
+
+</html>
+
